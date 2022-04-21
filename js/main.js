a2a34386d6e76d29a0505220cc7dcea68bf989f1
Sourav Dey <sourav.blackjack@gmail.com>
2022-04-21 12:58:55 +0530

    able to delete and edit (partially) the records, using HTML hidden field"

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
 const departments = [
-  { id: 100, name: 'Dept1' },
-  { id: 200, name: 'Dept2' },
-  { id: 300, name: 'Dept3' },
+  { id: 100, name: 'Dept 1' },
+  { id: 200, name: 'Dept 2' },
+  { id: 300, name: 'Dept 3' },
 ];
+
+/* state management */
+const state = {
+  personIdCounter: 0,
+  editingPerson: null,
+};
 
 class Person {
   constructor(name, age, deptId) {
@@ -23,20 +29,20 @@ class Person {
 
 const personData = [
   {
-    id: Date.now(),
+    id: ++state.personIdCounter,
     person: new Person('Ram', 29, 200),
   },
   {
-    id: Date.now(),
+    id: ++state.personIdCounter,
     person: new Person('Shyam', 22),
   },
   {
-    id: Date.now(),
+    id: ++state.personIdCounter,
     person: new Person('Jodu', 24, 100),
   },
   {
-    id: Date.now(),
-    person: new Person('Modu', 24),
+    id: ++state.personIdCounter,
+    person: new Person('Modu', 21),
   },
 ];
 
@@ -45,32 +51,229 @@ const removeContent = (id = 'div-content') => {
   div?.parentNode?.removeChild(div);
 };
 
-const insertPersonData = () => {};
+const addPerson = () => {};
 
-const updatePersonData = () => {};
+const decodeHiddenRowValue = string => {
+  const attrs = {};
 
-const deletePersonData = () => {};
+  string.split(';').forEach(ln => {
+    if (ln) {
+      const [k, v] = ln.split('=');
+      attrs[k] = v;
+    }
+  });
+
+  return attrs;
+};
+
+const updatePerson = data => {
+  const item = personData.find(p => p.id === data.id);
+  const deptId = departments.find(d => d.id === data.deptId)?.id;
+
+  if (item) item.person = new Person(data.name, data.age, deptId || null);
+  else
+    personData.push({
+      id: ++state.personIdCounter,
+      person: new Person(data.name, data.age, deptId || null),
+    });
+
+  console.log('updated person:', { personData, item, deptId, data });
+};
+
+const encodeHiddenRowValue = attrs => {
+  let string = '';
+
+  for (const key in attrs) string += `${key}=${attrs[key]};`;
+
+  return string;
+};
+
+const editPerson = (trId, personId) => {
+  // alert(`edit tr=${trId}, personId=${personId}`);
+  const tr = document.getElementById(trId);
+  const updatedPersonValue = { id: personId };
+  let hiddenAttrs;
+  if (tr.childNodes[0].id.startsWith('td-hidden-p'))
+    hiddenAttrs = decodeHiddenRowValue(tr.childNodes[0].value);
+
+  if (hiddenAttrs.mode === 'editing') return false;
+  tr.childNodes.forEach(node => {
+    console.log('node:', node);
+
+    if (!node.id.startsWith('td-id-p')) {
+      if (node.id.startsWith('td-hidden-p')) {
+        hiddenAttrs.mode = 'editing';
+        node.value = encodeHiddenRowValue(hiddenAttrs);
+      } else if (node.id.startsWith('td-name-p')) {
+        updatedPersonValue.name = node.innerText;
+        node.innerText = '';
+
+        node.appendChild(
+          createElement({
+            type: 'input',
+            id: `editing-${node.id}`,
+            mode: 'text',
+            value: updatedPersonValue.name,
+            onchange: event => {
+              console.log(
+                'value:',
+                event.target.value,
+                ', valid:',
+                !!event.target.value.match(/^[a-z ]{5,10}$/i)
+              );
+              if (event.target.value.match(/^[a-z ]{5,10}$/i)) {
+                event.target.className = 'valid-input';
+                hiddenAttrs.error = '';
+                tr.childNodes[0].value = encodeHiddenRowValue(hiddenAttrs);
+                updatedPersonValue.name = event.target.value;
+              } else {
+                event.target.className = 'invalid-input';
+                hiddenAttrs.error = 'Invalid name';
+                tr.childNodes[0].value = encodeHiddenRowValue(hiddenAttrs);
+              }
+            },
+            // onkeypress: event => {
+            //   let value = event.target.value;
+            //   if (event.key !== 'Enter') value += event.key;
+
+            //   event.target.className =
+            //     event.key === 'a' ? 'invalid-input' : 'valid-input';
+
+            //   console.log(
+            //     'onkeypress:',
+            //     value,
+            //     event,
+            //     event.key,
+            //     event.target.value,
+            //     // document.getElementById(`editing-${node.id}`).value,
+            //     'accept button:',
+            //     document.getElementById(`editing_accept-${node.id}`)
+            //   );
+            // },
+          })
+        );
+      } else if (node.id.startsWith('td-age-p')) {
+        updatedPersonValue.age = node.innerText;
+        node.innerText = '';
+
+        node.appendChild(
+          createElement({
+            type: 'input',
+            id: `editing-${node.id}`,
+            mode: 'number',
+            value: updatedPersonValue.age,
+            min: 18,
+            max: 60,
+            onchange: event => {
+              if (
+                event.target.value &&
+                +event.target.value >= 18 &&
+                +event.target.value <= 60
+              ) {
+                hiddenAttrs.error = '';
+                tr.childNodes[0].value = encodeHiddenRowValue(hiddenAttrs);
+                updatedPersonValue.age = event.target.value;
+                event.target.className = 'valid-input';
+              } else {
+                hiddenAttrs.error = 'Invalid age';
+                tr.childNodes[0].value = encodeHiddenRowValue(hiddenAttrs);
+                event.target.className = 'invalid-input';
+              }
+            },
+          })
+        );
+      } else if (node.id.startsWith('td-dept-p')) {
+        updatedPersonValue.deptId =
+          departments.find(d => d.name === node.innerText)?.id || 0;
+        node.innerText = '';
+
+        node.appendChild(
+          createElement({
+            type: 'select',
+            id: `editing-${node.id}`,
+            values: [{ id: 0, name: 'None' }, ...departments],
+            value: updatedPersonValue.deptId,
+            onchange: event => {
+              updatedPersonValue.deptId = event.target.value;
+            },
+          })
+        );
+      } else if (node.id.startsWith('td-delete-p')) {
+        console.log('delete img tr:', node, node.childNodes);
+        node.childNodes[0].remove();
+
+        // const wrapperDiv = createElement({
+        //   type: 'div',
+        //   id: `editing-${node.id}`,
+        // });
+        node.appendChild(
+          createElement({
+            type: 'img',
+            id: `editing_accept-${node.id}`,
+            src: './resources/tick.png',
+            alt: 'accept edit',
+            height: 17,
+            width: 17,
+            onclick: () => {
+              if (hiddenAttrs.error)
+                alert(
+                  `Error in person data with ID ${personId}: ${hiddenAttrs.error}`
+                );
+              else {
+                updatePerson(updatedPersonValue);
+
+                hiddenAttrs.mode = 'show';
+                hiddenAttrs.error = '';
+                tr.childNodes[0].value = encodeHiddenRowValue(hiddenAttrs);
+
+                loadPersonTable();
+
+                document.getElementById(
+                  'label-status'
+                ).innerText = `Person with ID ${personId} updated`;
+              }
+            },
+            className: 'edit-button',
+          })
+        );
+        node.appendChild(
+          createElement({
+            type: 'img',
+            id: `editing_decline-${node.id}`,
+            src: './resources/cross.png',
+            alt: 'decline edit',
+            height: 15,
+            width: 15,
+            onclick: () => alert('decline edit'),
+            className: 'edit-button',
+          })
+        );
+
+        // node.appendChild(wrapperDiv);
+      }
+    }
+  });
+};
+
+const deletePerson = id => {
+  if (confirm(`Delete person with ID ${id}?`)) {
+    personData.splice(
+      personData.findIndex(p => p.id === id),
+      1
+    );
+
+    loadPersonTable();
+
+    document.getElementById(
+      'label-status'
+    ).innerText = `Person with ID ${id} deleted`;
+  }
+};
 
 const loadPersonTable = () => {
   removeContent();
 
   const contentDiv = createElement({ type: 'div', id: 'div-content' });
-
-  const editDiv = createElement({ type: 'div', className: 'div-edit' });
-  [
-    { title: 'Insert', onclick: insertPersonData },
-    { title: 'Update', onclick: updatePersonData },
-    { title: 'Delete', onclick: deletePersonData },
-  ].forEach(button =>
-    editDiv.appendChild(
-      createElement({
-        type: 'button',
-        title: button.title,
-        onclick: button.onclick,
-      })
-    )
-  );
-  contentDiv.appendChild(editDiv);
 
   const para = createElement({
     type: 'p',
@@ -87,27 +290,71 @@ const loadPersonTable = () => {
       className: 'table',
     });
 
-    const tr = createElement({ type: 'tr', className: 'table-data' });
+    const tr = createElement({ type: 'tr' });
     ['ID', 'Name', 'Age (in years)', 'Department'].forEach(title =>
-      tr.appendChild(
-        createElement({ type: 'th', className: 'table-data', text: title })
-      )
+      tr.appendChild(createElement({ type: 'th', text: title }))
     );
     table.appendChild(tr);
 
     personData.forEach(data => {
-      const tr = createElement({ type: 'tr', className: 'table-data' });
+      const trId = `tr-p${data.id}`;
+      const tr = createElement({
+        type: 'tr',
+        id: trId,
+        ondblclick: () => editPerson(trId, data.id),
+      });
 
       [
-        data.id,
-        data.person.name,
-        data.person.age,
-        departments.find(d => d.id === data.person.deptId)?.name || 'None',
+        {
+          id: `td-hidden-p${data.id}`,
+          hidden: true,
+          value: 'mode=show;error=;',
+        },
+        { id: `td-id-p${data.id}`, value: data.id },
+        { id: `td-name-p${data.id}`, value: data.person.name },
+        { id: `td-age-p${data.id}`, value: data.person.age },
+        {
+          id: `td-dept-p${data.id}`,
+          value:
+            departments.find(d => d.id === data.person.deptId)?.name || 'None',
+        },
       ].forEach(info =>
         tr.appendChild(
-          createElement({ type: 'td', className: 'table-data', text: info })
+          createElement(
+            (() => {
+              const prop = { id: info.id };
+
+              if (info.hidden) {
+                prop.type = 'input';
+                prop.mode = 'hidden';
+                prop.value = info.value;
+              } else {
+                prop.type = 'td';
+                prop.text = info.value;
+              }
+
+              return prop;
+            })()
+          )
         )
       );
+
+      const tdCross = createElement({
+        type: 'td',
+        id: `td-delete-p${data.id}`,
+      });
+      tdCross.className = 'td-buttons';
+      tdCross.appendChild(
+        createElement({
+          type: 'img',
+          id: `img-delete-${data.id}`,
+          alt: 'delete',
+          height: 17,
+          src: './resources/bin.png',
+          onclick: () => deletePerson(data.id),
+        })
+      );
+      tr.appendChild(tdCross);
 
       table.appendChild(tr);
     });
@@ -120,7 +367,7 @@ const loadPersonTable = () => {
     'Data loaded at ' + new Date().toLocaleString();
 };
 
-const clearData = () => {
+const clearContent = () => {
   removeContent();
   document.getElementById('label-status').innerText = 'Content cleared';
 };
@@ -157,10 +404,17 @@ const elementProps = [
   // buttons elements
   {
     type: 'button',
-    id: 'button-load-data',
-    title: 'Load Data',
+    id: 'button-fetch-data',
+    title: 'Fetch',
     onclick: loadPersonTable,
   },
+  {
+    type: 'button',
+    id: 'button-add-data',
+    title: 'Add',
+    onclick: addPerson,
+  },
+
   // {
   //   type: 'button',
   //   id: 'button-show-data',
@@ -177,7 +431,7 @@ const elementProps = [
     type: 'button',
     id: 'button-clear',
     title: 'Clear',
-    onclick: clearData,
+    onclick: clearContent,
   },
 
   // labels
@@ -225,7 +479,70 @@ const createElement = prop => {
       element = document.createElement(prop.type);
       if (prop.id) element.id = prop.id;
       if (prop.className) element.className = prop.className;
+      else if (prop.type === 'tr' || prop.type === 'td' || prop.type === 'th')
+        element.className = 'table-data';
       if (prop.text) element.innerText = prop.text;
+      if (prop.ondblclick) element.ondblclick = prop.ondblclick;
+      break;
+
+    case 'img':
+      element = document.createElement('img');
+      if (prop.id) element.id = prop.id;
+      element.src = prop.src;
+      if (prop.alt) element.alt = prop.alt;
+      if (prop.height) element.height = prop.height;
+      if (prop.width) element.width = prop.width;
+      if (prop.className) element.className = prop.className;
+      if (prop.onclick) element.onclick = prop.onclick;
+      break;
+
+    // case 'textbox':
+    //   element = document.createElement('div');
+    //   if (prop.id) element.id = prop.id;
+    //   element.className = prop.className || 'textbox-container';
+
+    //   const input = document.createElement('input');
+    //   input.id = `input-text-${prop.id}`;
+    //   input.type = 'text';
+    //   input.value = prop.value;
+    //   if (prop.onkeypress) element.onkeypress = prop.onkeypress;
+    //   if (prop.onchange) element.onchange = prop.onchange;
+
+    //   const img = document.createElement('img');
+    //   img.id = `img-status-${prop.id}`;
+    //   img.src = './resources/red-warn.png';
+    //   img.height = 15;
+    //   img.width = 15;
+    //   img.alt = 'input-error';
+
+    //   element.appendChild(input);
+    //   element.appendChild(img);
+    //   break;
+
+    case 'input':
+      element = document.createElement('input');
+      if (prop.id) element.id = prop.id;
+      element.type = prop.mode;
+      if (prop.min !== null || prop.min !== undefined) element.min = prop.min;
+      if (prop.max !== null || prop.max !== undefined) element.max = prop.max;
+      if (prop.value) element.value = prop.value;
+      // if (prop.onkeypress) element.onkeypress = prop.onkeypress;
+      if (prop.onchange) element.onchange = prop.onchange;
+      if (prop.className) element.className = prop.className;
+      break;
+
+    case 'select':
+      element = document.createElement('select');
+      if (prop.id) element.id = prop.id;
+      if (prop.onchange) element.onchange = prop.onchange;
+      prop.values.forEach(v => {
+        const option = document.createElement('option');
+        option.value = v.id;
+        option.innerText = v.name;
+        option.selected = prop.value === v.id;
+        element.appendChild(option);
+      });
+      if (prop.className) element.className = prop.className;
       break;
 
     default:
